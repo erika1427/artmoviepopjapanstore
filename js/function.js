efee2a4897918e059f81397338bc459a5ab93dda
erika1427 <erika.tokuyama17@gmail.com>
2026-06-26 05:18:17 +0000

    gnavを完成

--- js/function.js
+++ js/function.js
@@ -1,9 +1,19 @@
-$(function() {
-// ここにjQueryを記述
+$(function () {
+    //スライドメニュー -----------------------------------------------------------
 
+    $('#btn__burger').on('click', function () { // 👈 修正1: #btn を #btn__burger に変更
+        $('#btn__top').toggleClass('rotateTop'); // 👈 修正2: #btn__burger を #btn__top に変更
+        $('#btn__middle').toggleClass('hideMiddle');
+        $('#btn__bottom').toggleClass('rotateBottom');
+        $('#gnav').toggleClass('translateNav');
+    });
 
-
-
-
-
-});
+    $('.gnav__link').on('click', function () {
+        if ($('#gnav').hasClass('translateNav')) {
+            $('#gnav').removeClass('translateNav');
+            $('#btn__top').removeClass('rotateTop');
+            $('#btn__middle').removeClass('hideMiddle');
+            $('#btn__bottom').removeClass('rotateBottom');
+        }
+    });
+})
